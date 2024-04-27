6eef48fcdc28248eac95eb871647913904aeeca1
raviraj3214 <raviraj32140@gmail.com>
2024-04-27 19:20:25 +0530

    hhj

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -43,7 +43,7 @@ router.put("/profile", requireSignIn, updateProfileController);
 //orders
 router.get("/orders", requireSignIn, getOrdersController);
 //razorpay order
-router.post("/razorpay-orders",requireSignIn, getRazOrdersController );
+router.get("/razorpay-orders",requireSignIn, getRazOrdersController );
 
 //all orders
 router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
